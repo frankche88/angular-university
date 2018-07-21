5f501f5127948fa276c0cf6765b4084d17b2ab7d
Frank Jonislla Pillaca <frank.che88@gmail.com>
2018-07-21 06:53:33 +0000

    http

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
 import { STUDENTS } from './mock-students'
 import { Student } from './student';
 import { MessageService } from './message.service';
 
-import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor(private messageService: MessageService) { }
+  private studentsUrl = 'api/alumnos';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getStudents(): Observable<Student[]> {
     this.messageService.add('StudentService: fetched students');
-    return of(STUDENTS);
+    return this.http.get<Student[]>(this.studentsUrl)
   }
 
   getStudent(id: number): Observable<Student> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(STUDENTS.find(student => student.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`StudentService: ${message}`);
   }
 
 }
